25a6e35c0d7097cb2ce1786ca23c6163cf30f08f
Mariem Derbali <mariem.derbali20@gmail.com>
2022-05-05 18:24:23 +0200

    05/05/2022

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -124,7 +124,6 @@ const routes = [
     { path: '/chefdepartement/demandes', exact: true, name: 'Demandes', component: Demandes },
     { path: '/chefdepartement/reunions', exact: true, name: 'Reunions', component: Reunions },
     { path: '/chefdepartement/calendrier', exact: true, name: 'Calendrier', component: Calendar },
-
     { path: '/chefdepartement/edit-event/:_id', exact: true, name: 'Editevent', component: Editevent },
 
 
