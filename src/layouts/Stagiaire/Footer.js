47bcfbc54664140cbea096709b27a428b0bfb06f
Mariem Derbali <mariem.derbali20@gmail.com>
2022-05-25 19:14:39 +0200

    25/05/2022

--- src/layouts/Stagiaire/Footer.js
+++ src/layouts/Stagiaire/Footer.js
@@ -3,6 +3,7 @@ import React from 'react'
 //Pied de page
 export default function Footer() {
     return (
+
         <div style={{ position: 'fixed', bottom: '0', left: '0', width: '100%', marginBottom: "15px" }}>
             <div className="row align-items-center justify-content-lg-between">
                 <div className="col-8 mx-auto text-center mt-1">
